9470a429a0eaa1479b906a1671831528731fb95e
DenisBytes <denisbytes@gmail.com>
2024-02-16 17:36:06 +0000

    [build]: kafka producer

--- wsserver/src/main/java/com/github/denisbytes/javafootball/wsserver/handlers/ServerHandler.java
+++ wsserver/src/main/java/com/github/denisbytes/javafootball/wsserver/handlers/ServerHandler.java
@@ -1,26 +1,41 @@
 package com.github.denisbytes.javafootball.wsserver.handlers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.denisbytes.javafootball.wsserver.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+
+@Component
 public class ServerHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private KafkaProducer kafkaProducer;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        System.out.println(session.getRemoteAddress());
+        System.out.println("Connection established with session ID: " + session.getId());
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
-        System.out.println(message.getPayload());
+        try {
+            kafkaProducer.sendMessage(message.getPayload());
+            System.out.println("Received JSON data from session ID " + session.getId() + ": " + message.getPayload());
+        } catch (Exception e) {
+            System.err.println("Error parsing JSON message: " + e.getMessage());
+        }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        System.out.println(status);
+        System.out.println("Connection closed with session ID: " + session.getId() + ", Status: " + status);
     }
 
 }
---
+++ wsserver/src/main/java/com/github/denisbytes/javafootball/wsserver/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.github.denisbytes.javafootball.wsserver.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+    private static final String TOPIC_NAME = "MatchHighlightsData";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info("Sending message to Kafka: {}", message);
+        kafkaTemplate.send(TOPIC_NAME, message);
+    }
+}
